4cf16e4f0364fb4f8de3163edc3cf61bb6fa3741
baekjung <zvgandam@gmail.com>
2019-06-18 16:51:42 +0900

    createAccount Mutation part Three

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -3,15 +3,19 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Mutation: {
     createAccount: async (_, args) => {
-      const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
-        username,
-        email,
-        firstName,
-        lastName,
-        bio
-      });
-      return user;
+      const { username, email, firstName = "", lastName = "" } = args;
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("This username is already taken");
+      } else {
+        await prisma.createUser({
+          username,
+          email,
+          firstName,
+          lastName
+        });
+        return true;
+      }
     }
   }
 };
